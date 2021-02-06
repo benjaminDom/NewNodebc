3eb28b7c9e45287b411666a6540312a76d773723
ben <58023164+benjaminDom@users.noreply.github.com>
2021-02-06 13:37:16 -0600

    Add files via upload

---
+++ jsnode/js/index.js
@@ -0,0 +1,24 @@
+console.log(2+3/7);   
+console.log("This is a mess");   
+var x=10;
+var y=2;
+
+var z=x+y;
+
+console.log("this is z=",z);
+console.log("this is x="+x+", and  this is y="+y);
+
+
+//-------data  types  
+
+let undefinedVar;
+let undefinedVar2=undefined;
+let undefinedVar4="";
+let Bool1=true;
+let Bool2=false;
+console.log("undefinedVar:",undefinedVar);
+console.log("undefinedVar2:",undefinedVar2);
+console.log("bool1:",Bool1);
+console.log("Bool2",Bool2);
+
+
